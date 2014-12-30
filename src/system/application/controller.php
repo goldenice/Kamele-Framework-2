96ee0340e35453db340354ba8f0dd081cc8bd0d6
Rick Lubbers <ik@ricklubbers.nl>
2014-12-30 16:45:13 +0100

    Implemented default view code in the controller

--- src/system/application/controller.php
+++ src/system/application/controller.php
@@ -12,4 +12,50 @@ if (!defined('SYSTEM')) exit('No direct script access allowed');
  * @author		Rick Lubbers <me@ricklubbers.nl>
  */
 abstract class Controller extends Application {
+    
+    protected $view = false;
+    protected $view_obj = null;
+    protected $view_path = "";
+    protected $view_data = array();
+    
+    /**
+     * Constructor function
+     * @return  void
+     */
+    public function __construct() {
+        $this->loadView();
+    }
+    
+    /**
+     * Loads view when variables are correctly set
+     * @return  boolean
+     */
+    protected function _loadView() {
+        if ($this->view == true) {
+            $this->view_obj = new View($view_path);
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Outputs the view to the browser if it is set
+     * @return  boolean
+     */
+    protected function _outputView() {
+        if ($this->view == true && $this->view_obj instanceof \System\Core\View) {
+            $this->view_obj->output($this->view_data);
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Destructor function
+     * @return  void
+     */
+    public function __destruct() {
+        $this->renderView();
+    }
+    
 }
