622436ee754ddbb56fc57f5ab3cf0818c53458c6
Rick Lubbers <ik@ricklubbers.nl>
2015-03-20 16:54:39 +0100

    Fixing the broken build (hopefully)

--- tests/system/database/drivers/mssql/querybuildermssqltest.php
+++ tests/system/database/drivers/mssql/querybuildermssqltest.php
@@ -123,7 +123,7 @@ class QueryBuilderMssqlTest extends PHPUnit_Framework_Testcase {
     }
     
     public function testComplex() {
-    	$this->qb1->select('*')->from('y')->where(array('a', 'b'))->order(array('c'))->limit(3);
+    	$this->qb1->select('*')->from('y')->where(array('a', 'b'))->orderBy(array('c'))->limit(3);
     	$qb1 = "SELECT TOP 3 FROM `y` WHERE `a`='b' ORDER BY `c` ASC";
     	
     	$this->assertEquals($qb1, trim($this->qb1->getQuery()), "Testing complex query");
