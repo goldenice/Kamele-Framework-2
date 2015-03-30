a9a22a813fad6c17fc235394d134a2c43783272b
Rick Lubbers <ik@ricklubbers.nl>
2015-03-30 14:59:37 +0200

    Added KTL exceptions

---
+++ src/system/exceptions/ktl/ktlexception.php
@@ -0,0 +1,8 @@
+<?php
+namespace System\Exceptions\Ktl;
+
+if (!defined('SYSTEM')) exit('No direct script access allowed');
+
+
+class KtlException extends \Exception {
+}
---
+++ src/system/exceptions/ktl/syntaxexception.php
@@ -0,0 +1,8 @@
+<?php
+namespace System\Exceptions\Ktl;
+
+if (!defined('SYSTEM')) exit('No direct script access allowed');
+
+
+class SyntaxException extends \System\Exceptions\Ktl\KtlException {
+}
